1982b5c8686a59b87196700a4da4e9a9f9cc1b2d
Ketryn Parkonen <ketrynparkonen@gmail.com>
2025-09-30 09:47:37 +0300

    add math examples

--- script.js
+++ script.js
@@ -1,37 +1,30 @@
-console.log('hello world!');
-var num = 1; // don't use var anymore 
-let num2 = 2; 
-console.log(num2, num);
-num = 3.5;
-console.log(typeof num); 
-num = 3;
-console.log(typeof num);
-let text = 'Ketryn'; 
-console.log(typeof text); 
-let bool = true;
-console.log(typeof bool); 
-bool = false; 
-console.log(typeof bool); 
-let nothing;
-console.log(typeof nothing);
-nothing = undefined; 
-nothing = null; 
-console.log(typeof nothing);
-let array = [1, 2, 3, 4, 'sad', true, 3.5];
-console.log(typeof array);
-let obj = {
-    name: 'Ketryn',
-    age: 26,
-    cats: ['Loki'],
-    data: {
-        phone: '12345678',
-        email: 'ketryn.parkonen@tptlive.ee'
-    }
-};
-console.log(typeof obj);
-
-function hello() {
-    console.log('hello');
-}
-
-console.log(typeof hello);
+console.log(1 + 2);
+let num = 1; 
+console.log(num);
+num = num + 2; 
+console.log(num);
+num = num - 1; 
+console.log(num);
+num = num * 2
+console.log(num);
+num = num / 3;
+console.log(num);
+num = 3; 
+num = num ** 3;
+console.log(num);
+num = 9; 
+num = num % 2; // modulo 
+console.log(num);
+num += 2; 
+num -= 2;
+num *= 2; 
+num /= 2;
+num **= 2; 
+num %= 2; 
+console.log(num);
+num++;
+console.log(num);
+num--;
+console.log(num);
+num = Math.sqrt(9);
+console.log(num);
