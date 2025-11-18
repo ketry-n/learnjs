ffac14581d7d8a2f49a8fc8b841c2ab788a63fcb
Ketryn Parkonen <ketrynparkonen@gmail.com>
2025-11-18 09:54:22 +0200

    vue

--- script.js
+++
@@ -1,9 +0,0 @@
-let nums = [1,2,3,4,5,6,7,8,9];
-
-nums.forEach(num=> console.log(num));
-nums.forEach((num,i) => console.log(num,i));
-
-let even = nums.filter(num => num%2 === 0);
-console.log(even);
-let sum = nums.reduce((sum, num) => sum+num);
-console.log(sum);
